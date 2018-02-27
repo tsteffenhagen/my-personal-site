22e8f7cbdc928037d5f614d6f7aa01dc26c0f1fd
Tyler Steffenhagen <steffenhagen.tyler@gmail.com>
2018-02-26 21:17:21 -0600

    Brought in react-bootstrap and updated Carousel

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -3,6 +3,11 @@ import FA from 'react-fontawesome';
 import Footer from '../Footer/Footer';
 import image from '../../images/test.png';
 import image2 from '../../images/Banner.png';
+import { Carousel } from 'react-bootstrap';
+import { Grid } from 'react-bootstrap'
+import { Row } from 'react-bootstrap'
+import { Col } from 'react-bootstrap'
+
 
 
 
@@ -12,41 +17,32 @@ export default class AboutPage extends Component {
   render() {
     return (
       <div>
-        <div className="container">
-          <div className="row">
-            <div className="col">
-            <h1>This is the about page </h1>
-        </div>
-            <div className="col">
-              2 of 2
-        </div>
-          </div>
-          <div className="row justify-content-md-center">
-            <div className="col">
-              <div id="aboutCarousel" className="carousel slide" data-ride="carousel">
-                <div className="carousel-inner" role="listbox">
-                  <div className="carousel-item active">
-                    <img className="d-block img-fluid" src={image} alt="" />
-                  </div>
-                  <div className="carousel-item">
-                    <img className="d-block img-fluid" src={image2} alt="Second Slide" />
-                  </div>
-                  {/* <div className="carousel-item">
-                    <img className="d-block img-fluid" src={image} alt="Third Slide" />
-                  </div> */}
-                </div>
-                <a className="carousel-control-prev" href="#aboutCarousel" role="button" data-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="sr-only">Previous</span>
-                </a>
-                <a className="carousel-control-next" href="#aboutCarousel" role="button" data-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="sr-only">Next</span>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Grid className="fluid">
+          <Row>
+            <Col md={6}>
+              <h1>This is the about page </h1>
+            </Col>
+            <Col md={6}>
+              <p>2 of 2</p>
+        </Col>
+          </Row>
+          <Row>
+            <Col md={3}>
+            </Col>
+            <Col md={6}>
+          <Carousel>
+            <Carousel.Item>
+              <img width={900} height={400} alt="" src={image} />
+
+            </Carousel.Item>
+            <Carousel.Item>
+              <img width={900} height={400} alt="" src={image2} />
+
+            </Carousel.Item>
+          </Carousel>
+          </Col>
+          </Row>
+        </Grid>
         <Footer />
       </div>
     )
--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,23 @@ import logo from '../logo.svg';
 import { Link } from 'react-router-dom';
 import './App.css';
 import FA  from 'react-fontawesome';
+import {Button} from 'react-bootstrap'
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <h1 className="App-title">Tyler Steffenhagen</h1>
           <div>
-            <Link to="" className="btn btn-sm"><h5>Home</h5></Link>
+            <Link to=""><Button bsStyle="link"><h5>Home</h5></Button></Link>
             {"|"}
-            <Link to="about" className="btn btn-sm"><h5>About page</h5></Link>
+            <Link to="about"><Button bsStyle="link"><h5>About Page</h5></Button></Link>
             {"|"}
-            <Link to="resume" className="btn btn-sm"><h5>Resume</h5></Link>
+            <Link to="resume"><Button bsStyle="link"><h5>Resume</h5></Button></Link>
           </div>
         </header>
       </div>
